ba52830b7576502832a9d9c13d58d69345efba97
JeffersonTracinkas <jefferson.tracinkas@gmail.com>
2019-12-13 17:15:40 -0300

    service e dependencias

--- src/main/java/com/example/curso/resources/UserResource.java
+++ src/main/java/com/example/curso/resources/UserResource.java
@@ -1,19 +1,35 @@
 package com.example.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.curso.entities.User;
+import com.example.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		var user = new User();
+	public ResponseEntity<List<User>> findAll(){
+		var users = userService.findAll();
+		
+		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		var user = userService.findById(id);
+		
 		return ResponseEntity.ok().body(user);
 	}
 	
